c171381d2887f79dc952a402e14eebe85e47bfdb
Buchos <pluyart@gmail.com>
2020-06-16 12:29:41 +0200

    ajout index phase 1

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+var charOneConfirm = document.getElementById("charOneConfirm");
+var startFight = document.getElementById("startFight");
+var charOneConfirm = document.getElementById("charOneConfirm");
+var charTwoConfirm = document.getElementById("charTwoConfirm");
+
+function showMore() {
+    charOneConfirm.style.display="none";
+    startFight.style.display="block";
+}
