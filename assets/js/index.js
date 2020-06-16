f742b329b573001240f9aae5609e448fb4db2ad4
Buchos <pluyart@gmail.com>
2020-06-16 14:33:24 +0200

    ajout index création perso

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,16 @@
-var charOneConfirm = document.getElementById("charOneConfirm");
-var startFight = document.getElementById("startFight");
-var charOneConfirm = document.getElementById("charOneConfirm");
-var charTwoConfirm = document.getElementById("charTwoConfirm");
+//Use this script to generate your character
+function Person(race,item){
+    this.race = race;
+    this.item = item;
+    this.currenthealth = 100;
+    this.maxHealth = 100;
+    
+    this.min = 3;
+    this.maxDamage = 20;
+    this.maxHealing = 30;
 
-function showMore() {
-    charOneConfirm.style.display="none";
-    startFight.style.display="block";
+
+    let charOneName = document.getElementById("charOneName").value;
+    let charTwoName = document.getElementById("charTwoName").value;
+
 }
