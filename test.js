fb94962bd600e569a3180e7b21b8da19c23984e7
artiart <art.kouznetsov@gmail.com>
2020-06-18 12:04:16 +0200

    wtf

---
+++ test.js
@@ -0,0 +1,7 @@
+document.getElementById("charOneHPBar").style["width"] = 100 + "%";
+alert("click ok to modify HP");
+document.getElementById("charOneHPBar").style["width"] = 20 + "%";
+alert("click ok to modify HP");
+document.getElementById("charOneHPBar").style["width"] = 80 + "%";
+alert("click ok to modify HP");
+document.getElementById("charOneHPBar").style["width"] = 10 + "%";
