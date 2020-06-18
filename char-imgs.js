fad839f0f4a171a79086f311deadf0f799050056
artiart <art.kouznetsov@gmail.com>
2020-06-18 16:28:45 +0200

    Create Player Img Changer

---
+++ char-imgs.js
@@ -0,0 +1,52 @@
+// Au lancement de la page game.html
+
+const playerBlueImg = document.getElementById("playerBlueImg");
+const playerRedImg = document.getElementById("playerRedImg");
+
+// s'assurer que les img ont bien l'ID playerBlueImg et playerRedImg (deja fait dans mon html)
+
+playerBlueImg.setAttribute("src", "assets/img/js-rpg-human.png");
+playerBlueImg.setAttribute("src", "assets/img/js-rpg-elf.png");
+playerBlueImg.setAttribute("src", "assets/img/js-rpg-orc.png");
+playerBlueImg.setAttribute("src", "assets/img/js-rpg-vampire.png");
+
+
+function setPlayerBlueImg(character) {
+    switch (character.race) {
+        case "human":
+            playerBlueImg.setAttribute("src", "assets/img/js-rpg-human.png");
+            break;
+        case "elf":
+            playerBlueImg.setAttribute("src", "assets/img/js-rpg-elf.png");
+            break;
+        case "orc":
+            playerBlueImg.setAttribute("src", "assets/img/js-rpg-orc.png");
+            break;
+        case "vampire":
+            playerBlueImg.setAttribute("src", "assets/img/js-rpg-vampire.png");
+            break;
+    }
+}
+
+function setPlayerRedImg(character) {
+    switch (character.race) {
+        case "human":
+            playerRedImg.setAttribute("src", "assets/img/js-rpg-human.png");
+            break;
+        case "elf":
+            playerRedImg.setAttribute("src", "assets/img/js-rpg-elf.png");
+            break;
+        case "orc":
+            playerRedImg.setAttribute("src", "assets/img/js-rpg-orc.png");
+            break;
+        case "vampire":
+            playerRedImg.setAttribute("src", "assets/img/js-rpg-vampire.png");
+            break;
+    }
+}
+
+var characterBlue = {name: "Arthur", race: "human"};
+var characterRed = {name: "Christophe", race: "elf"};
+console.log(characterBlue);
+setPlayerBlueImg(characterBlue);
+setPlayerRedImg(characterRed);
