b717450753ac4dc3296ae3f52d9b79882eeda5fb
artiart <art.kouznetsov@gmail.com>
2020-06-19 09:52:04 +0200

    working on win screen

---
+++ win-round.js
@@ -0,0 +1,10 @@
+var wonRound;
+wonRound = 0;
+
+function winLose () {
+    if (charBlue.HP > charRed.HP) {
+        document.getElementById("")
+    }
+}
+
+// mettre la fonction winLose a l'endroit ou l'on teste si HP<=0
